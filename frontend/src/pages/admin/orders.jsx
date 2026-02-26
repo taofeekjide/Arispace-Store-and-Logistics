a4830eebdc97e04caa5e8f5f21f6df11387bee91
Tao02 <idowutaofeek16@gmail.com>
2026-02-26 05:37:30 +0100

    Admin Orders

--- frontend/src/pages/admin/orders.jsx
+++ frontend/src/pages/admin/orders.jsx
@@ -1,5 +1,5 @@
 
-import AdminOrders from '../../components/admin/orders';
+import AdminOrders from '../../components/admin/orders.jsx';
 import React from 'react'
 
 export default function AdminOrdersPage() {
